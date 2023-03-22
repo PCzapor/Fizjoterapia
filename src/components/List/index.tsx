5ceda6e03c8ea7237899e368113eab00a10adcd2
Piotr <p.czapor@gmail.com>
2023-03-22 19:36:02 +0100

    hope works now

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -8,72 +8,51 @@ const ListComponent = () => {
   const [isActive2, setIsActive2] = useState<boolean>();
   function handleClick() {
     setIsActive(!isActive);
-    console.log({ isActive });
   }
   function handleClick1() {
     setIsActive1(!isActive1);
-    console.log({ isActive1 });
   }
   function handleClick2() {
     setIsActive2(!isActive2);
-    console.log({ isActive2 });
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.listBox}>
-        <h2
-          onClick={() => {
-            handleClick();
-          }}
-        >
+        <h2 onClick={handleClick}>
           WYKSZTAŁCENIE <span>{isActive ? "Zwiń" : "Więcej"}</span>
         </h2>
         {WYKSZTALCENIE.map((item, i) => {
           return (
-            <div key={i} className={styles.item}>
-              <ul className={isActive ? styles.desc : styles.descHidden}>
-                <li key={i}>{item}</li>
-              </ul>
-            </div>
+            <ul key={i} className={isActive ? styles.desc : styles.descHidden}>
+              <li>{item}</li>
+            </ul>
           );
         })}
       </div>
       <div className={styles.listBox}>
-        <h2
-          onClick={() => {
-            handleClick1();
-          }}
-        >
+        <h2 onClick={handleClick1}>
           KURSY
           <span>{isActive1 ? "Zwiń" : "Więcej"}</span>
         </h2>
         {KURSY.map((item, i) => {
           return (
-            <div key={i} className={styles.item}>
-              <ul className={isActive1 ? styles.desc : styles.descHidden}>
-                <li key={i}>{item}</li>
-              </ul>
-            </div>
+            <ul key={i} className={isActive1 ? styles.desc : styles.descHidden}>
+              <li>{item}</li>
+            </ul>
           );
         })}
       </div>
       <div className={styles.listBox}>
-        <h2
-          onClick={() => {
-            handleClick2();
-          }}
-        >
+        <h2 onClick={handleClick2}>
           WARSZTATY
           <span>{isActive2 ? "Zwiń" : "Więcej"}</span>
         </h2>
         {WARSZTATY.map((item, i) => {
           return (
-            <div key={i} className={styles.item}>
-              <ul className={isActive2 ? styles.desc : styles.descHidden}>
-                <li key={i}>{item}</li>
-              </ul>
-            </div>
+            <ul key={i} className={isActive2 ? styles.desc : styles.descHidden}>
+              <li>{item}</li>
+            </ul>
           );
         })}
       </div>
